4837098238eaf7ab1d8f6f1b3b7f643a36da20b9
Allister <redtahr@users.noreply.github.com>
2017-08-16 10:22:04 +1200

    browser window and garbage collection

--- src/main.js
+++ src/main.js
@@ -3,3 +3,19 @@ console.log('main!')
 const electron = require('electron')
 
 const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+let mainWindow
+
+app.on('ready', _ => {
+  console.log('ready!')
+  mainWindow = new BrowserWindow({
+    height:400,
+    width:400
+  })
+
+  mainWindow.on('closed', _ => {
+    console.log('closed!')
+    mainWindow = null // so the garbage collector knows it can collect this
+  })
+})
