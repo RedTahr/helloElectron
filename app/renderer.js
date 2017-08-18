8e16e83130e755d053bbf848d817c4f7169badc5
Allister <redtahr@users.noreply.github.com>
2017-08-18 14:56:38 +1200

    wouldn't it be nice if nativescript-nfc worked for me, if not will try the nfc-pcsc files route.

--- app/renderer.js
+++ app/renderer.js
@@ -1,7 +1,34 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var os = require("os");
+var nativescript_nfc_1 = require("nativescript-nfc");
 console.log("renderer fired.");
+var nfc = new nativescript_nfc_1.Nfc();
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('platform-info').innerHTML = os.platform();
+});
+nfc.available().then(function (avail) {
+    console.log(avail ? "Yes" : "No");
+});
+nfc.setOnTagDiscoveredListener(function (data) {
+    console.log("Discovered a tag with ID " + data.id);
+}).then(function () {
+    console.log("OnTagDiscovered listener added");
+});
+nfc.setOnTagDiscoveredListener(null).then(function () {
+    console.log("OnTagDiscovered listener removed");
+});
+nfc.setOnNdefDiscoveredListener(function (data) {
+    if (data.message) {
+        for (var m in data.message) {
+            var record = data.message[m];
+            console.log("Ndef discovered! Message record: " + record.payloadAsString);
+        }
+    }
+}).then(function () {
+    console.log("OnNdefDiscovered listener added");
+});
+nfc.setOnNdefDiscoveredListener(null).then(function () {
+    console.log("OnNdefDiscovered listener removed");
 });
 //# sourceMappingURL=renderer.js.map
