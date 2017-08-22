03a4e02fc08afb853f6918dfa7f4622fa6104c8a
Allister <redtahr@users.noreply.github.com>
2017-08-23 10:34:37 +1200

    added handy utility functions.

--- app/renderer.js
+++ app/renderer.js
@@ -1,5 +1,5 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+//Object.defineProperty(exports, "__esModule", { value: true });
 var os = require("os");
 console.log("renderer fired.");
 document.addEventListener('DOMContentLoaded', function () {
